c83920b86f4d9c8dfe4befdaede478671d60dbe1
arfan.tkp103@digital.telkom.co.id <arfan.febriyanto@digital.telkom.co.id>
2025-02-20 15:39:21 +0700

    Initial Commit

---
+++ watchcount Watch App/watchcountApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchcountApp.swift
+//  watchcount Watch App
+//
+//  Created by Arfan on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct watchcount_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchcount Watch AppTests/watchcount_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  watchcount_Watch_AppTests.swift
+//  watchcount Watch AppTests
+//
+//  Created by Arfan on 20/02/25.
+//
+
+import Testing
+@testable import watchcount_Watch_App
+
+struct watchcount_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
